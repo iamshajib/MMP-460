f5b988917ed3e0ee5260c6f98349ce24fde1aca5
iamshajib <Mflsssbus12d>
2016-03-11 21:55:32 -0800

    adding image

--- wordpress(new)/wp-content/themes/bare-bones-bootstrap/custom-home.php
+++ wordpress(new)/wp-content/themes/bare-bones-bootstrap/custom-home.php
@@ -83,13 +83,12 @@
                  </div>
 
 
-                <div class="row" style="margin-top: 3%; margin-left: 7%;">
+                <div class="row" style="margin-top: 3%;">
 
 
+<!--  *******************Query Post********************  -->
                     <div class="col-md-4 col-lg-4">
 
-
-                        <!--         *******************Query Post********************      -->
                         <?php
 // the query
 $the_query = new WP_Query( array( 'cat' => 4 ) ); ?>
@@ -129,24 +128,10 @@ $the_query = new WP_Query( array( 'cat' => 4 ) ); ?>
                     <div class="col-md-4 col-lg-4"><img class="img-responsive" src="http://placehold.it/200x150" alt="" /></div>
 
                 </div>
-                <div class="row" style="margin-top: 5%; margin-left: 7%;">
-                    <div class="col-md-6 col-lg-6"><img class="img-responsive" src="http://wpmmp.bmcc.cuny.edu/~mshajib/mmp460/wp-content/themes/bare-bones-bootstrap/image/1.jpg" alt="" /></div>
-                    <div class="col-md-6 col-lg-6"><img class="img-responsive" src="http://wpmmp.bmcc.cuny.edu/~mshajib/mmp460/wp-content/themes/bare-bones-bootstrap/image/2.jpg" alt="" /></div>
-                </div>
-                <div class="row" style="margin-top: 1%; margin-left: 7%;">
-                    <div class="col-md-6 col-lg-6 ">
 
-                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
 
-                    </div>
-                    <div class="col-md-6 col-lg-6">
+            </div> <!-- end container-->
 
-                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-                    </div>
-                </div>
-            </div>
-        <!-- end container-->
 
 
 
