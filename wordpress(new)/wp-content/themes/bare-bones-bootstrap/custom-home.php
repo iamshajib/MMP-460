871e903dc3af2e5e479e6df5bef16d361b7f8e9b
iamshajib <Mflsssbus12d>
2016-03-22 10:45:13 -0700

    edit post text

--- wordpress(new)/wp-content/themes/bare-bones-bootstrap/custom-home.php
+++ wordpress(new)/wp-content/themes/bare-bones-bootstrap/custom-home.php
@@ -77,43 +77,6 @@
                 </div>
 
 
-                <div class="col-sm-12 col-md-12 col-lg-12" id="query-post">
-
-                                            <?php
-// the query
-$the_query = new WP_Query( array( 'cat' => 4 ) ); ?>
-
-                            <?php if ( $the_query->have_posts() ) : ?>
-
-                                <!-- pagination here -->
-
-                                <!-- the loop -->
-                                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                                    <!-- check if the post has a Post Thumbnail assigned to it. -->
-                                    <?php if ( has_post_thumbnail() ) : ?>
-                                        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-                                            <?php  the_post_thumbnail( 'post-featured', array( 'class' => 'img-responsive' ) );?>
-                                        </a>
-                                        <?php endif; ?>
-                                            <?php endwhile; ?>
-                                                <!-- end of the loop -->
-
-                                                <!-- pagination here -->
-
-                                                <?php wp_reset_postdata(); ?>
-
-                                                    <?php else : ?>
-                                                        <p>
-                                                            <?php _e( 'Sorry, no posts matched your criteria.' ); ?>
-                                                        </p>
-                                                        <?php endif; ?>
-
-
-
-                </div>
-                <!--***********End of Query Post******-->
-
-
 
 
                 <div class="col-sm-12 col-md-12 col-lg-12 home-featured">
@@ -152,13 +115,6 @@ $the_query = new WP_Query( array( 'cat' => 4 ) ); ?>
         </div>
         <!-- end container-->
 
-        <div class="container-fluid">
-            <div class="row before-footrer">
-                <div class="col-sm-4 col-md-4 col-lg-4">dsds</div>
-                <div class="col-sm-4 col-md-4 col-lg-4">dsdsds</div>
-                <div class="col-sm-4 col-md-4 col-lg-4">dsdsds</div>
-            </div>
-        </div>
 
 </div>   <!-- end of wrapper -->
 
