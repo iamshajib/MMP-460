1001e14789db9c9d13ad8767e9b959521b872651
iamshajib <Mflsssbus12d>
2016-03-25 15:09:29 -0700

    edit

--- wordpress(new)/wp-content/themes/bare-bones-bootstrap/custom-home.php
+++ wordpress(new)/wp-content/themes/bare-bones-bootstrap/custom-home.php
@@ -2,62 +2,62 @@
     <?php get_header(); ?>
 
 
-<div class="wrapper">
+        <div class="wrapper">
 
 
-        <div class="container-fluid background-home">
+            <div class="container-fluid background-home">
 
-            <div class="row" id="background-home-text">
+                <div class="row" id="background-home-text">
 
-                <div class="col-sm-12 col-md-12 com-lg-12">
+                    <div class="col-sm-12 col-md-12 com-lg-12">
 
-                    <h1 class="text-center"> BELIEVE IN</h1>
+                        <h1 class="text-center"> PORTFOLIO OF</h1>
 
-                    <h2 class="text-center">what you know </h2>
+                        <h2 class="text-center">Mohammed E Shajib </h2>
 
-                    <p class="text-center">HTML <samp>-</samp> | <samp>-</samp> CSS <samp>-</samp> | <samp>-</samp> JAVASCRIPT<samp>-</samp> |<samp>-</samp> ANIMATION<samp>-</samp> |<samp>-</samp> DESIGN
-                        <br>
-                        <br>
-                        <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        <br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                    </p>
+                        <p class="text-center">HTML <samp>-</samp> | <samp>-</samp> CSS <samp>-</samp> | <samp>-</samp> JAVASCRIPT<samp>-</samp> |<samp>-</samp> ANIMATION<samp>-</samp> |<samp>-</samp> DESIGN
+                            <br>
+                            <br>
+                            <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            <br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                        </p>
+
+                    </div>
 
                 </div>
 
-            </div>
+                <div class="row" id="scroll">
 
-            <div class="row" id="scroll">
-
-                <a class="mouse-scroll" href="#">
-                    <span class="mouse">
+                    <a class="mouse-scroll" href="#">
+                        <span class="mouse">
     <span class="mouse-movement">
     </span>
-                    </span>
-                    <span class="mouse-message fadeIn">scroll</span>
-                </a>
-
-            </div>
-
-
-
-        </div>
-        <!--end of background-->
-
-
-
-        <div class="container-fluid">
-
-            <div class="row" id="home-description">
-
-                <div class="col-sm-12 col-md-7 col-lg-7" id="intro-img" style="">
-                    <img class="img-responsive" src="http://portfolio.bmcc.is/mohammed.shajib/wp-content/uploads/2016/03/home-intro.png" alt="home-intro" width="600" height="400">
+                        </span>
+                        <span class="mouse-message fadeIn">scroll</span>
+                    </a>
 
                 </div>
 
-                <div class="col-sm-12 col-md-5 col-lg-5" id="writing" style="">
-                    <h1>Portfolio of <br> Mohammed Shajib</h1>
-                    <p>
-This is going to be my own portfolio, where I'll put some of my own works. Including my class works, and the works I have done from my passion. Instate of making a website for someone else I thought I better make my own to keep my works all together in cloud. That will be one of the secure ways to keep my work for future references. And most importantly as a multimedia student I am going to need a portfolio in future. It will be an easier way to show my works to others. <span class="hidden-xs">Also share it if somebody needs. That’s how my audience and me will also get benefit from it. Right now my intended audience will be my classmate and my friends.</p>
+
+
+            </div>
+            <!--end of background-->
+
+
+
+            <div class="container-fluid">
+
+                <div class="row" id="home-description">
+
+                    <div class="col-sm-12 col-md-7 col-lg-7" id="intro-img" style="">
+                        <img class="img-responsive" src="http://portfolio.bmcc.is/mohammed.shajib/wp-content/uploads/2016/03/home-intro.png" alt="home-intro" width="600" height="400">
+
+                    </div>
+
+                    <div class="col-sm-12 col-md-5 col-lg-5" id="writing" style="">
+                        <h1>Few facts about <br> me.</h1>
+                        <p>
+                            This is going to be my own portfolio, where I'll put some of my own works. Including my class works, and the works I have done from my passion. Instate of making a website for someone else I thought I better make my own to keep my works all together in cloud. That will be one of the secure ways to keep my work for future references. And most importantly as a multimedia student I am going to need a portfolio in future. It will be an easier way to show my works to others. <span class="hidden-xs">Also share it if somebody needs. That’s how my audience and me will also get benefit from it. Right now my intended audience will be my classmate and my friends.</p>
 
                 </div>
 
@@ -73,8 +73,8 @@ This is going to be my own portfolio, where I'll put some of my own works. Inclu
             <div class="row" style="margin-top: 3%; margin-bottom: 3%">
 
                 <div class="text-center" id="home-works-intro">
-                    <h1> My latest works</h1>
-                    <h4> class & outside</h4>
+                    <h1> My Latest Works</h1>
+                    <h4> class & Fieldwork </h4>
                 </div>
 
 
@@ -88,6 +88,13 @@ This is going to be my own portfolio, where I'll put some of my own works. Inclu
 
                 </div>
 
+                                <div class="col-sm-12 col-md-12 col-lg-12 home-featured">
+                    <h1>Coding</h1>
+                    <p>Game, JavScript, P5JS</p>
+                    <a href=" http://portfolio.bmcc.is/mohammed.shajib/2016/02/24/game/" target="_blank"><img class="img-responsive" src="http://portfolio.bmcc.is/mohammed.shajib/wp-content/uploads/2016/03/home-work3-2.jpg" alt="" /> </a>
+
+                </div>
+
                 <div class="col-sm-12 col-md-12 col-lg-12 home-featured">
                     <h1> Animation</h1>
                     <p>Adobe Flash & After Effect</p>
@@ -96,14 +103,7 @@ This is going to be my own portfolio, where I'll put some of my own works. Inclu
                 </div>
 
                 <div class="col-sm-12 col-md-12 col-lg-12 home-featured">
-                    <h1>Game</h1>
-                    <p>JavScript, P5JS</p>
-                    <a href=" http://portfolio.bmcc.is/mohammed.shajib/2016/02/24/game/" target="_blank"><img class="img-responsive" src="http://portfolio.bmcc.is/mohammed.shajib/wp-content/uploads/2016/03/home-work3-2.jpg" alt="" /> </a>
-
-                </div>
-
-                <div class="col-sm-12 col-md-12 col-lg-12 home-featured">
-                    <h1>Graphis</h1>
+                    <h1>Graphics</h1>
                     <p>Adobe Photoshop,
                         <br>Illustrator, inDesign</p>
                     <a href=" http://portfolio.bmcc.is/mohammed.shajib/2016/03/15/graphis/" target="_blank"><img class="img-responsive" src="http://portfolio.bmcc.is/mohammed.shajib/wp-content/uploads/2016/03/home-work4-1.jpg" alt="" /> </a>
