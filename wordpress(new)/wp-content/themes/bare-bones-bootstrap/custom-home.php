5fc4f5cb8a5e20d35ce726694596d902ac5febcf
iamshajib <Mflsssbus12d>
2016-05-22 00:20:09 -0700

    edit

--- wordpress(new)/wp-content/themes/bare-bones-bootstrap/custom-home.php
+++ wordpress(new)/wp-content/themes/bare-bones-bootstrap/custom-home.php
@@ -15,11 +15,7 @@
 
                         <h2 class="text-center">Mohammed E Shajib </h2>
 
-                        <p class="text-center">HTML <samp>-</samp> | <samp>-</samp> CSS <samp>-</samp> | <samp>-</samp> JAVASCRIPT<samp>-</samp> |<samp>-</samp> ANIMATION<samp>-</samp> |<samp>-</samp> DESIGN
-                            <br>
-                            <br>
-                            <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            <br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                        <p class="text-center"><a href=" http://portfolio.bmcc.is/mohammed.shajib/2016/03/08/web-projects/">Web Design </a><samp>-</samp> | <samp>-</samp> <a href=" http://portfolio.bmcc.is/mohammed.shajib/2016/02/24/game/">Programming</a> <samp>-</samp> | <samp>-</samp> <a href=" http://portfolio.bmcc.is/mohammed.shajib/2016/03/15/animations/"> Animation</a><samp>-</samp> |<samp>-</samp><a href=" http://portfolio.bmcc.is/mohammed.shajib/2016/03/15/graphis/"> Design</a>
                         </p>
 
                     </div>
@@ -49,15 +45,15 @@
 
                 <div class="row" id="home-description">
 
-                    <div class="col-sm-12 col-md-7 col-lg-7" id="intro-img" style="">
-                        <img class="img-responsive" src="http://portfolio.bmcc.is/mohammed.shajib/wp-content/uploads/2016/03/home-intro.png" alt="home-intro" width="600" height="400">
+                    <div class="col-sm-5 col-md-5 col-lg-5" id="intro-img" style="">
+                        <img class="img-responsive" src="http://portfolio.bmcc.is/mohammed.shajib/wp-content/uploads/2016/03/home-intro.png" alt="home-intro" >
 
                     </div>
 
-                    <div class="col-sm-12 col-md-5 col-lg-5" id="writing" style="">
-                        <h1>Few facts about <br> me.</h1>
-                        <p>
-                            This is going to be my own portfolio, where I'll put some of my own works. Including my class works, and the works I have done from my passion. Instate of making a website for someone else I thought I better make my own to keep my works all together in cloud. That will be one of the secure ways to keep my work for future references. And most importantly as a multimedia student I am going to need a portfolio in future. It will be an easier way to show my works to others. <span class="hidden-xs">Also share it if somebody needs. That’s how my audience and me will also get benefit from it. Right now my intended audience will be my classmate and my friends.</p>
+                    <div class="col-sm-5 col-sm-offset-2 col-md-5 col-lg-5" id="writing" style="">
+                        <h1>Facts about me.</h1>
+                        <p>My name is Mohammed Enamul Haque Shajib; I am a Multimedia Programming and Design student at Borough of Manhattan Community College. I am passionate about programming and interested in doing creative things. Trying to convert my passion into my profession.
+                        </p>
 
                 </div>
 
@@ -70,44 +66,43 @@
 
         <div class="container">
 
-            <div class="row" style="margin-top: 3%; margin-bottom: 3%">
+            <div class="row" style="margin-top: 2%; margin-bottom: 1%">
 
                 <div class="text-center" id="home-works-intro">
                     <h1> My Latest Works</h1>
-                    <h4> class & Fieldwork </h4>
+                    <h4> Class & Fieldwork </h4>
                 </div>
+            </div>
 
+             <div class="row" style="margin-bottom: 2%" id="home-work">
 
-
-
-                <div class="col-sm-12 col-md-12 col-lg-12 home-featured">
+                <div class="col-sm-12 col-md-6 col-lg-6 home-featured">
+                     <a href=" http://portfolio.bmcc.is/mohammed.shajib/2016/03/08/web-projects/">
                     <h1>Web Design</h1>
-                    <p>HTML, CSS, JavaScript
-                        <br> Bootstrap, P5JS</p>
-                    <a href=" http://portfolio.bmcc.is/mohammed.shajib/2016/03/08/web-projects/" target="_blank"><img class="img-responsive" src="http://portfolio.bmcc.is/mohammed.shajib/wp-content/uploads/2016/03/experiment-img.jpg" alt="" /> </a>
-
+                   <img class="img-responsive" src="http://portfolio.bmcc.is/mohammed.shajib/wp-content/uploads/2016/03/home-work2.jpg" alt="" />
+                    </a>
                 </div>
 
-                                <div class="col-sm-12 col-md-12 col-lg-12 home-featured">
-                    <h1>Coding</h1>
-                    <p>Game, JavScript, P5JS</p>
-                    <a href=" http://portfolio.bmcc.is/mohammed.shajib/2016/02/24/game/" target="_blank"><img class="img-responsive" src="http://portfolio.bmcc.is/mohammed.shajib/wp-content/uploads/2016/03/home-work3-2.jpg" alt="" /> </a>
-
+                <div class="col-sm-12 col-md-6 col-lg-6 home-featured">
+                     <a href=" http://portfolio.bmcc.is/mohammed.shajib/2016/02/24/game/">
+                    <h1>Programming</h1>
+                   <img class="img-responsive" src="http://portfolio.bmcc.is/mohammed.shajib/wp-content/uploads/2016/03/home-work3-2.jpg" alt="" />
+                    </a>
                 </div>
 
-                <div class="col-sm-12 col-md-12 col-lg-12 home-featured">
+
+                <div class="col-sm-12 col-md-6 col-lg-6 home-featured">
+                     <a href=" http://portfolio.bmcc.is/mohammed.shajib/2016/03/15/animations/">
                     <h1> Animation</h1>
-                    <p>Adobe Flash & After Effect</p>
-                    <a href=" http://portfolio.bmcc.is/mohammed.shajib/2016/03/15/animations/" target="_blank"><img class="img-responsive" src="http://portfolio.bmcc.is/mohammed.shajib/wp-content/uploads/2016/03/home-work1.jpg" alt="" /> </a>
-
+                   <img class="img-responsive" src="http://portfolio.bmcc.is/mohammed.shajib/wp-content/uploads/2016/03/home-work1.jpg" alt="" />
+                    </a>
                 </div>
 
-                <div class="col-sm-12 col-md-12 col-lg-12 home-featured">
-                    <h1>Graphics</h1>
-                    <p>Adobe Photoshop,
-                        <br>Illustrator, inDesign</p>
-                    <a href=" http://portfolio.bmcc.is/mohammed.shajib/2016/03/15/graphis/" target="_blank"><img class="img-responsive" src="http://portfolio.bmcc.is/mohammed.shajib/wp-content/uploads/2016/03/home-work4-1.jpg" alt="" /> </a>
-
+                <div class="col-sm-12 col-md-6 col-lg-6 home-featured">
+                    <a href=" http://portfolio.bmcc.is/mohammed.shajib/2016/03/15/graphis/">
+                    <h1>Design</h1>
+                    <img class="img-responsive" src="http://portfolio.bmcc.is/mohammed.shajib/wp-content/uploads/2016/03/home-work4-1.jpg" alt="" />
+                    </a>
                 </div>
 
             </div>
